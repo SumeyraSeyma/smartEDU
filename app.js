615c0d73b25b46476de24172a33c6212c8411213
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-12-01 15:02:47 +0300

    createCourse, getAllCourses and course

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -15,9 +16,12 @@ app.set("view engine", "ejs");
 
 //Middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
--- models/Course.js
+++ models/Course.js
@@ -1,19 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Please provide a course name'],
-        unique: true
-    },
-    description: {
-        type: String,
-        required: [true, 'Please provide a description']
-    },
-    createdAt : {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: [true, "Please provide a course name"],
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide a description"],
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  slug: {
+    type: String,
+    unique: true,
+  }
 });
+
+CourseSchema.pre('validate', function(next){
+  this.slug = slugify(this.name, {
+    lower: true,
+    strict: true
+  });
+  next();
+});
+
+const Course = mongoose.model("Course", CourseSchema);
+module.exports = Course;
