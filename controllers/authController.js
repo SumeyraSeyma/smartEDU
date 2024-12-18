d3ee9332b8b45f32cc20a35a9223ad4684b3c0dd
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-12-18 12:22:06 +0300

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
@@ -7,6 +8,26 @@ exports.createUser = async (req, res) => {
       status: "success",
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Invalid credentials",
+      });
+    }
+    res.status(200).send("Logged in");
   } catch (error) {
     res.status(400).json({
       status: "fail",
