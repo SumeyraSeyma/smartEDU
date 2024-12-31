2345b1440c4b2393cc6094c7878e09951387d075
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-12-31 12:34:48 +0300

    delete user and admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const { validationResult } = require("express-validator");
 const User = require("../models/User");
 const Category = require("../models/Category");
 const Course = require("../models/Course");
+const { useResolvedPath } = require("react-router-dom");
 
 exports.createUser = async (req, res) => {
   try {
@@ -53,10 +54,25 @@ exports.getDashboardPage = async (req, res) => {
   const user = await User.findById(req.session.userID).populate("courses");
   const categories = await Category.find();
   const courses = await Course.find({ user: req.session.userID });
+  const users = await User.find();
   res.status(200).render("dashboard", { 
     page_name: "dashboard", 
     user,
     categories,
-    courses 
+    courses,
+    users 
   });
+}
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    await Course.deleteMany({ user: req.params.id });
+    res.status(200).redirect("/users/dashboard");
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 }
